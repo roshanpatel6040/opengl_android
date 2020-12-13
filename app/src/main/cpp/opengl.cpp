861fa9c549d8d4f25b4edda3db1a01fd88a9da8f
Roshan Patel <Roshan.borana@gmail.com>
2020-12-13 14:54:54 +0530

    Draw Pyramid

--- app/src/main/cpp/opengl.cpp
+++ app/src/main/cpp/opengl.cpp
@@ -39,139 +39,6 @@ void readObj(const char *path) {
     free(buffer);
 }
 
-int loadShader(GLenum type, const std::string &shaderCode) {
-    GLuint shader = glCreateShader(type);
-    const char *source = shaderCode.c_str();
-    glShaderSource(shader, 1, &source, nullptr);
-    glCompileShader(shader);
-    if (shader == -1) {
-        __android_log_print(ANDROID_LOG_ERROR, "Shader", "%d", shader);
-    }
-    return shader;
-}
-
-void drawPyramid() {
-    float vertices[] = {
-            0.0f, 1.0f, 0.0f,
-            -1.0f, 0.0f, -1.0f,
-            1.0f, 0.0f, -1.0f,
-            1.0f, 0.0f, 1.0f,
-            -1.0f, 0.0f, 1.0f,
-    };
-    // Text chords before texture at 4 points(direction)
-    // values are fixed 0.0f to 1.0f
-    float texChords[] = {
-            0.0f, 1.0f, // to map texture bottom left
-            0.0f, 0.0f, // to map texture bottom right
-            1.0f, 0.0f,// to map texture top right
-            1.0f, 1.0f,// to map texture top left
-    };
-
-    // order floats for drawing triangles
-    // Re use number for anti clockwise
-    // This order will create two triangles which makes it square
-//    unsigned int order[] = {1, 2, 3, 1, 3, 4, 1, 4, 5, 1, 5, 2, 3, 2, 4, 2, 5, 4};
-    unsigned int order[] = {0, 4, 3, 0, 3, 2, 0, 2, 1, 0, 1, 4, 4, 2, 1, 4, 3, 2};
-    // RGBA color format
-    float color[] = {0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f};
-    int CHORDS_PER_VERTEX = 3;
-
-    // Works from bottom
-    glm::mat4 ortho = glm::ortho(-1.0f, (float) 1.0, -1.0f, (float) 1.0, -1.0f,
-                                 100.0f);
-    glm::mat4 perspective = glm::perspective(glm::radians(
-            90.0f), // The vertical Field of View, in radians: the amount of "zoom". Think "camera lens". Usually between 90° (extra wide) and 30° (quite zoomed in)
-                                             (float) windowWidth /
-                                             (float) windowHeight,       // Aspect Ratio. Depends on the size of your window. Notice that 4/3 == 800/600 == 1280/960, sounds familiar ?
-                                             0.1f,              // Near clipping plane. Keep as big as possible, or you'll get precision issues.
-                                             100.0f             // Far clipping plane. Keep as little as possible.
-    );
-    // Translation
-    glm::mat4 translation = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-    // Scaling
-    glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(3.0f));
-    // Rotation
-    glm::mat4 rotationMatrixX = rotate(glm::mat4(1.0f), glm::radians(5.0f),
-                                       glm::vec3(1.0f, 0.0f, 0.0f));
-    glm::mat4 rotationMatrixY = rotate(glm::mat4(1.0f), glm::radians(45.0f),
-                                       glm::vec3(0.0f, 1.0f, 0.0f));
-    // Camera matrix
-    glm::mat4 cameraMatrix = glm::lookAt(
-            glm::vec3(0.0f, 0.0f, 3.0f), // the position of your camera, in world space
-            glm::vec3(0.0f, 0.0f, 0.0f), // where you want to look at, in world space
-            glm::vec3(0.0f, 1.0f,
-                      0.0f) // probably glm::vec3(0,1,0), but (0,-1,0) would make you looking upside-down, which can be great too
-    );
-    // Model matrix
-    glm::mat4 modelMatrix = translation * scaleMatrix;
-    // final projection
-    glm::mat4 projection = perspective;
-
-    string vertexTextureShader = "attribute vec3 position;\n"
-                                 //                                 "attribute vec2 texChords;\n"
-                                 "uniform mat4 u_MVP;\n"
-                                 //                                 "varying vec2 chords;\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 " gl_Position = vec4(position,1.0);\n"
-                                 //                                 " chords = texChords;\n"
-                                 "}";
-
-    string fragmentTextureShader = "uniform vec4 color[2];\n" // Passing multiple rgba
-                                   //                                   "uniform sampler2D u_texture;\n"
-                                   //                                   "varying vec2 chords;\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   //                                   " vec4 textureColor = texture2D(u_texture,chords);\n"
-                                   //                                   " gl_FragColor = textureColor;\n" // assign it to texture color
-                                   " gl_FragColor = color[0];\n" // select which color is selected from array
-                                   "}";
-
-    // Enable blending
-    GLCall(glEnable(GL_DEPTH_TEST))
-    GLCall(glCullFace(GL_BACK))
-    GLCall(glEnable(GL_BLEND))
-    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA))
-
-    GLCall(GLuint program = glCreateProgram())
-    GLCall(glAttachShader(program, loadShader(GL_VERTEX_SHADER, vertexTextureShader)))
-    GLCall(glAttachShader(program, loadShader(GL_FRAGMENT_SHADER, fragmentTextureShader)))
-    GLCall(glLinkProgram(program))
-    GLCall(glUseProgram(program))
-
-    GLCall(GLint positionHandle = glGetAttribLocation(program, "position"))
-    GLCall(glEnableVertexAttribArray(positionHandle))
-    GLCall(glVertexAttribPointer(positionHandle, 3 /* co-ordinates per vertex */, GL_FLOAT, false,
-                                 0, vertices))
-
-    GLCall(GLint colorHandle = glGetUniformLocation(program, "color"))
-    GLCall(glUniform4fv(colorHandle, 2, color)) // Passing two rgba
-
-    GLCall(glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, order))
-
-//    GLCall(GLint texChordsHandle = glGetAttribLocation(program, "texChords"))
-//    GLCall(glEnableVertexAttribArray(texChordsHandle))
-//    GLCall(glVertexAttribPointer(texChordsHandle, 3 /* co-ordinates per vertex */, GL_FLOAT, false,0, texChords))
-
-//    GLCall(GLint projectionHandle = glGetUniformLocation(program, "u_MVP"))
-//    GLCall(glUniformMatrix4fv(projectionHandle, 1 /* No. of matrix array in our case only 1*/,
-//                              false,
-//                              glm::value_ptr(
-//                                      projection) /* pass reference at 0 0 position other work will be handled by opengl itself Basically opengl will take other values automatically */))
-
-    // Texture code and its uniform location
-    // Active texture slot is 0
-    // "/storage/emulated/0/Pictures/UHD Wallpapers/_uhdminimal34.jpg"
-//    GLuint mReferenceID = drawTextures(
-//            "/storage/emulated/0/Pictures/UHD Wallpapers/_uhdminimal34.jpg", 0);
-//    GLCall(GLint texturePosition = glGetUniformLocation(program, "u_texture"))
-//    GLCall(glUniform1i(texturePosition, 0)) // slot passed as 0
-
-    GLCall(glDisableVertexAttribArray(positionHandle))
-    // delete texture
-//    GLCall(glDeleteTextures(GL_TEXTURE_2D, &mReferenceID))
-}
-
 /**
  * Draw triangle
  */
@@ -179,7 +46,7 @@ void drawTriangle() {
     int CHORDS_COLOR_PER_VERTEX = 4;
     int BYTES_PER_FLOAT = 4;
     float vertices[] = {
-        //    X    Y     Z     R     G      B    A
+            //    X    Y     Z     R     G      B    A
             0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f,
             -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f};
@@ -227,10 +94,13 @@ void drawTriangle() {
     GLCall(glDrawArrays(GL_TRIANGLES, 0, 3))
     GLCall(shader.disableVertexAttribPointer(positionHandle))
     GLCall(shader.disableVertexAttribPointer(colorPositionHandle))
-    shader.unBind();
-    vb.unBind();
+    GLCall(shader.unBind())
+    GLCall(vb.unBind())
 }
 
+/**
+ * Draw square
+ */
 void drawSquare() {
     // x, y, z coordinates system
     // Here y = 0 determines bottom left of the screen
@@ -372,12 +242,124 @@ void drawSquare() {
     GLCall(ib.unBind())
 }
 
+/**
+ * Draw Pyramid
+ */
+void drawPyramid() {
+    float vertices[] = {
+            0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+            -1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+            1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+            -1.0f, 0.0f, 1.0f, 0.7f, 0.3f, 0.5f, 1.0f,
+    };
+    // Text chords before texture at 4 points(direction)
+    // values are fixed 0.0f to 1.0f
+    float texChords[] = {
+            0.0f, 1.0f, // to map texture bottom left
+            0.0f, 0.0f, // to map texture bottom right
+            1.0f, 0.0f,// to map texture top right
+            1.0f, 1.0f,// to map texture top left
+    };
+
+    // order floats for drawing triangles
+    // Re use number for anti clockwise
+    unsigned int order[] = {0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 1, 2, 1, 3, 1, 4, 3};
+    // RGBA color format
+    float color[] = {0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f};
+    // Works from bottom
+    glm::mat4 ortho = glm::ortho(-1.0f, (float) 1.0, -1.0f, (float) 1.0, -1.0f,
+                                 100.0f);
+    glm::mat4 perspective = glm::perspective(glm::radians(
+            100.0f), // The vertical Field of View, in radians: the amount of "zoom". Think "camera lens". Usually between 90° (extra wide) and 30° (quite zoomed in)
+                                             (float) windowWidth /
+                                             (float) windowHeight,       // Aspect Ratio. Depends on the size of your window. Notice that 4/3 == 800/600 == 1280/960, sounds familiar ?
+                                             0.1f,              // Near clipping plane. Keep as big as possible, or you'll get precision issues.
+                                             50.1f             // Far clipping plane. Keep as little as possible.
+    );
+    // Translation
+    glm::mat4 translation = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+    // Scaling
+    glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(3.0f));
+    // Rotation
+    glm::mat4 rotationMatrixX = rotate(glm::mat4(1.0f), glm::radians(30.0f),
+                                       glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::mat4 rotationMatrixY = rotate(glm::mat4(1.0f), glm::radians(45.0f),
+                                       glm::vec3(0.0f, 1.0f, 0.0f));
+    // Camera matrix
+    glm::mat4 cameraMatrix = glm::lookAt(
+            glm::vec3(0.0f, 0.0f, 3.0f), // the position of your camera, in world space
+            glm::vec3(0.0f, 0.0f, 0.0f), // where you want to look at, in world space
+            glm::vec3(0.0f, 1.0f,
+                      0.0f) // probably glm::vec3(0,1,0), but (0,-1,0) would make you looking upside-down, which can be great too
+    );
+    // Model matrix
+    glm::mat4 modelMatrix = translation * scaleMatrix;
+    // final projection
+    glm::mat4 projection = perspective * cameraMatrix * modelMatrix;
+
+    string vertexTextureShader = "attribute vec3 position;\n"
+                                 "attribute vec4 color;\n"
+                                 "varying vec4 v_color;\n"
+                                 //                                 "attribute vec2 texChords;\n"
+                                 "uniform mat4 u_MVP;\n"
+                                 //                                 "varying vec2 chords;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 " v_color = color;\n"
+                                 " gl_Position = u_MVP * vec4(position,1.0);\n"
+                                 //                                 " chords = texChords;\n"
+                                 "}";
+
+    string fragmentTextureShader = "uniform vec4 color[2];\n" // Passing multiple rgba
+                                   "varying vec4 v_color;\n"
+                                   //                                   "uniform sampler2D u_texture;\n"
+                                   //                                   "varying vec2 chords;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   //                                   " vec4 textureColor = texture2D(u_texture,chords);\n"
+                                   //                                   " gl_FragColor = textureColor;\n" // assign it to texture color
+                                   " gl_FragColor = v_color;\n" // select which color is selected from array
+                                   "}";
+    GLCall(glEnable(GL_DEPTH_TEST))
+    GLCall(GLuint program = glCreateProgram())
+    GLCall(Shader shader(program, vertexTextureShader, fragmentTextureShader))
+    GLCall(glLinkProgram(program))
+    GLCall(VertexBuffer vb(vertices, sizeof(vertices)))
+    GLCall(IndexBuffer ib(order, sizeof(order)))
+    GLCall(glUseProgram(program))
+
+    GLCall(GLint positionHandle = shader.getAttributeLocation("position"))
+    GLCall(shader.vertexAttribPointer(positionHandle, GL_FLOAT, 3, 7 * sizeof(float),
+                                      (GLvoid *) nullptr))
+    GLCall(shader.enableVertexAttribArray(positionHandle))
+
+    GLCall(GLint colorPositionHandle = shader.getAttributeLocation("color"))
+    GLCall(shader.vertexAttribPointer(colorPositionHandle, GL_FLOAT, 4, 7 * sizeof(float),
+                                      (GLvoid *) (3 * sizeof(float))))
+    GLCall(shader.enableVertexAttribArray(colorPositionHandle))
+
+    GLCall(GLint projectionHandle = shader.getUniformLocation("u_MVP"))
+    GLCall(shader.setUniformMatrix4fv(projectionHandle, 1, glm::value_ptr(projection)))
+
+    GLCall(GLint colorHandle = shader.getUniformLocation("color"))
+    GLCall(shader.setUniform4fv(colorHandle, 2, color))
+
+//    GLCall(glDrawElements(GL_TRIANGLES, ib.getCount(), GL_UNSIGNED_INT, nullptr))
+    GLCall(glDrawArrays(GL_TRIANGLES, 0, ib.getCount()))
+    GLCall(shader.disableVertexAttribPointer(positionHandle))
+    GLCall(shader.disableVertexAttribPointer(colorPositionHandle))
+    GLCall(shader.unBind())
+    GLCall(vb.unBind())
+    GLCall(ib.unBind())
+}
+
 extern "C" {
 void Java_com_demo_opengl_Render_render(JNIEnv *env, jobject object) {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     drawSquare();
     drawTriangle();
-//    drawPyramid();
+    drawPyramid();
 }
 
 void Java_com_demo_opengl_Render_onSurfaceCreated(JNIEnv *env, jobject object) {
